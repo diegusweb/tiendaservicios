e40a3fccac9f1b988b286350119191e38c3a3c98
Diego Rueda <diego.rueda2@gmail.com>
2021-08-11 15:31:45 -0400

    postgress services

---
+++ TiendaServicios.Api.Autor/Application/ConsultaFiltro.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TiendaServicios.Api.Autor.Models;
+using TiendaServicios.Api.Autor.Persistencia;
+
+namespace TiendaServicios.Api.Autor.Application
+{
+    public class ConsultaFiltro
+    {
+        public class AutorUnico : IRequest<AutorDto> { 
+            public string AutorGuid { get; set; }
+        }
+
+        public class Manejador : IRequestHandler<AutorUnico, AutorDto>
+        {
+            private readonly ContextoAutor _contexto;
+
+            private readonly IMapper _mapper;
+
+            public Manejador(ContextoAutor contexto, IMapper mapper) {
+                _contexto = contexto;
+                _mapper = mapper;
+            }
+            public async Task<AutorDto> Handle(AutorUnico request, CancellationToken cancellationToken)
+            {
+                var autor = await _contexto.AutorLibros.Where(x => x.AutorLibroGuid == request.AutorGuid).FirstOrDefaultAsync();
+
+                if (autor == null) {
+                    throw new Exception("NO se encuentra autor");
+                }
+
+                var autorDto = _mapper.Map<AutorLibro, AutorDto>(autor);
+
+                return autorDto;
+            }
+        }
+    }
+}
---
+++ TiendaServicios.Api.Autor/Persistencia/ContextoAutor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TiendaServicios.Api.Autor.Models;
+
+namespace TiendaServicios.Api.Autor.Persistencia
+{
+    public class ContextoAutor : DbContext
+    {
+        public ContextoAutor(DbContextOptions<ContextoAutor> options) : base(options)
+        {
+        }
+
+        public DbSet<AutorLibro> AutorLibros { get; set; }
+
+        public DbSet<GradoAcademico> GradoAcademicos { get; set; }
+    }
+}
