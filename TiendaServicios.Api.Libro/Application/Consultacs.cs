23520c2adfa6be5fb382a42e6166de12be51f4ea
Diego Rueda <diego.rueda2@gmail.com>
2021-08-11 18:07:26 -0400

    services for sqlserver

---
+++ TiendaServicios.Api.Libro/Application/Consultacs.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TiendaServicios.Api.Libro.Models;
+using TiendaServicios.Api.Libro.Persistencia;
+
+namespace TiendaServicios.Api.Libro.Application
+{
+    public class Consultacs
+    {
+        public class Ejecuta : IRequest<List<LibroMaterialDto>>
+        { 
+        
+        }
+
+        public class Manejador : IRequestHandler<Ejecuta, List<LibroMaterialDto>>
+        {
+            private readonly ContextoLibreria _contexto;
+
+            private readonly IMapper _mapper;
+
+            public Manejador(ContextoLibreria contexto, IMapper mapper) 
+            {
+                _contexto = contexto;
+                _mapper = mapper;
+            }
+
+            public async Task<List<LibroMaterialDto>> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var libros = await _contexto.LibreriaMateria.ToListAsync();
+               var librosDto = _mapper.Map<List<LibreriaMaterial>, List<LibroMaterialDto>>(libros);
+
+                return librosDto;
+            }
+        }
+    }
+}
---
+++ TiendaServicios.Api.Libro/Application/Nuevo.cs
@@ -0,0 +1,61 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TiendaServicios.Api.Libro.Models;
+using TiendaServicios.Api.Libro.Persistencia;
+
+namespace TiendaServicios.Api.Libro.Application
+{
+    public class Nuevo
+    {
+        public class Ejecuta : IRequest
+        { 
+            public string Titulo { get; set; }
+
+            public DateTime? FechaPublicacion { get; set; }
+
+            public Guid? AutorLibro { get; set; }
+        }
+
+        public class EjecutaValidacion : AbstractValidator<Ejecuta>
+        {
+            public EjecutaValidacion() {
+                RuleFor(x => x.Titulo).NotEmpty();
+                RuleFor(x => x.FechaPublicacion).NotEmpty();
+                RuleFor(x => x.AutorLibro).NotEmpty();
+            }
+        }
+
+        public class Manejador : IRequestHandler<Ejecuta>
+        {
+            private readonly ContextoLibreria _contexto;
+
+            public Manejador(ContextoLibreria contexto)
+            {
+                _contexto = contexto;
+            }
+
+            public async Task<Unit> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var libro = new LibreriaMaterial
+                {
+                    Titulo = request.Titulo,
+                    AutorLibro = request.AutorLibro,
+                    FechaPublicaion = request.FechaPublicacion
+                };
+
+                _contexto.LibreriaMateria.Add(libro);
+                var value = await _contexto.SaveChangesAsync();
+
+                if (value > 0)
+                    return Unit.Value;
+
+                throw new Exception("No se pudo insertar");
+            }
+        }
+    }
+}
