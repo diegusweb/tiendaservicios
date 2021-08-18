566d2e2e816461c3565cde7cf7a30cc69c20dcf6
Diego Rueda <diego.rueda2@gmail.com>
2021-08-18 10:42:00 -0400

    project to mysql

---
+++ TiendaServicios.Api.CarritoCompra/Application/Nuevo.cs
@@ -0,0 +1,66 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TiendaServicios.Api.CarritoCompra.Models;
+using TiendaServicios.Api.CarritoCompra.Persistencia;
+
+namespace TiendaServicios.Api.CarritoCompra.Application
+{
+    public class Nuevo
+    {
+        public class Ejecuta : IRequest
+        {
+            public DateTime? FechaCreacion { get; set; }
+
+            public List<string> ProductoLista { get; set; }
+        }
+
+        public class Manejador : IRequestHandler<Ejecuta>
+        {
+            private readonly CarritoContexto _contexto;
+
+            public Manejador(CarritoContexto contexto)
+            {
+                _contexto = contexto;
+            }
+
+            public async Task<Unit> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var carritoSesion = new CarritoSesion
+                {
+                    FechaCreacion = request.FechaCreacion
+                };
+
+                _contexto.CarritoSesion.Add(carritoSesion);
+                var value = await _contexto.SaveChangesAsync();
+
+                if (value == 0) {
+                    throw new Exception("Errore en la insercion del carrito de compra");
+                }
+
+                int id = carritoSesion.CarritoSesionId;
+
+                foreach (var obj in request.ProductoLista)
+                {
+                    var detalleSesion = new CarritoSesionDetalle
+                    {
+                        FechaCreacion = DateTime.Now,
+                        CarritoSesionId = id,
+                        ProductoSeleccionado = obj
+                    };
+
+                    _contexto.CarritoSesionDetalle.Add(detalleSesion);
+                }
+
+                value = await _contexto.SaveChangesAsync();
+                if (value > 0)
+                    return Unit.Value;
+
+                throw new Exception("Nose pudo insertar el detalle de carrito de compras");
+            }
+        }
+    }
+}
